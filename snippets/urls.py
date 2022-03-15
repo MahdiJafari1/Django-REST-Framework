df0e6a5b87846960ffcf19515b46f2ef912f9165
mahdi <mahdi100th@gmail.com>
2022-03-15 10:01:19 +0430

    part3: class-based views completed

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,7 +4,7 @@ from snippets import views
 
 urlpatterns = [
     path("snippets/", views.SnippetList.as_view(), name="snippet_list"),
-    path("snippets/<int:pk>", views.SnippetDetail.as_view(), name="snippet")
+    path("snippets/<int:pk>", views.SnippetDetail.as_view(), name="snippet"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
