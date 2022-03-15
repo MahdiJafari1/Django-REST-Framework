4e8dd4c4a259f0a95ec1d24f8c12dc222cb39bf6
mahdi <mahdi100th@gmail.com>
2022-03-15 10:09:22 +0430

    model migrated

--- snippets/models.py
+++ snippets/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from pygments import highlight
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
+from pygments.lexers import get_lexer_by_name
+from pygments import highlight
+from pygments.formatters.html import HtmlFormatter
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
@@ -17,8 +19,24 @@ class Snippet(models.Model):
         choices=LANGUAGE_CHOICES, default="python", max_length=100
     )
     style = models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
-    owner = models.ForeignKey("auth.User", related_name=_("snippets"), on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        "auth.User", related_name="snippets", on_delete=models.CASCADE
+    )
     highlighted = models.TextField()
+
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = "table" if self.linenos else False
+        options = {"title": self.title} if self.title else {}
+        formatter = HtmlFormatter(
+            style=self.style, linenos=linenos, full=True, **options
+        )
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ["created"]
